da4591022c1661232143f00c57ba8d270f32f0da
Janhavi Maurya <janhavimaurya8738@gmail.com>
2025-10-22 10:51:42 +0530

    my Demo App

---
+++ myDemoApp/src/main/java/org/demo/myDemoApp/App.java
@@ -0,0 +1,13 @@
+package org.demo.myDemoApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
